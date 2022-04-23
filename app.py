baeb4c2458d1e1c484a98d3c1c17b71b4fb6e9df
Guillaume Arp <guillaume.arp@gmail.com>
2022-04-23 14:15:25 +0200

    updated streamlit app

--- app.py
+++ app.py
@@ -4,23 +4,21 @@ import numpy as np
 import plotly.express as px
 import requests
 import os
-from dotenv import load_dotenv
 import datetime
-
-load_dotenv()
-key = os.getenv('APIKEY')
 
 st.set_page_config(page_title='Kayak', page_icon=':sunny:', layout='wide')
 
 st.markdown("<h1 style='text-align: center;'>Projet Kayak</h1>", unsafe_allow_html=True)
 
-@st.cache()
+key = os.getenv('APIKEY')
+mapbox_token = os.getenv('MAPBOX_TOKEN')
+
 def load_data():
     'Loading data...'
     return pd.read_csv('df_complete.csv')
 
 def get_weather_data(df):
-    # Getting the weather data based on coordinates from the Openweathermap API
+
     df_full = df.copy(deep=True)
     df_full.drop(['felt_temperature', 'rain_chances', 'humidity', 'score_weather'], axis=1, inplace=True)
     temps_list = []
@@ -31,7 +29,7 @@ def get_weather_data(df):
         lat = i.lat
         lon = i.lon
         r = requests.get(f"https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&units=metric&appid={key}").json()
-        weather_7_days = r['daily'][1:] # Getting the weather data for the next 7 days, first item is the current weather, which we don't want here
+        weather_7_days = r['daily'][1:] 
         temps = [j['feels_like']['day'] for j in weather_7_days]
         rain = [int(j['pop'] * 100) for j in weather_7_days]
         humidity = [j['humidity'] for j in weather_7_days]
@@ -42,7 +40,6 @@ def get_weather_data(df):
     df_full['felt_temperature'] = temps_list
     df_full['rain_chances'] = rain_pop
     df_full['humidity'] = humidity_list
-    # Calculation of a weather quality indicator, using absolute value of 35 - temperature (with a weight of 2), chances of rain as a percentage, and humidity as a percentage (with a weight of 0.5) - lower is better
     df_full['score_weather'] = df_full.apply(lambda x: abs((35 - np.mean(x['felt_temperature'])) * 2) + np.mean(x['rain_chances']) + (np.mean(x['humidity']) / 2), axis=1)
     
     return df_full
@@ -70,21 +67,16 @@ def hotel_data(df):
 
 def weather_data(df):
 
-    df_weather = df_s3[['id', 'city', 'lat', 'lon', 'day_plus', 'felt_temperature', 'rain_chances', 'humidity', 'score_weather']]
-
+    df_weather = df[['id', 'city', 'lat', 'lon', 'day_plus', 'felt_temperature', 'rain_chances', 'humidity', 'score_weather']]
     df_weather_final = pd.DataFrame()
-    df_weather_final[['id', 'city', 'lat', 'lon', 'score_weather']] = df_weather[['id', 'city', 'lat', 'lon', 'score_weather']]
+    df_weather_final[['id', 'city', 'lat', 'lon', 'felt_temperature', 'rain_chances', 'humidity', 'score_weather']] = df_weather[['id', 'city', 'lat', 'lon', 'felt_temperature', 'rain_chances', 'humidity', 'score_weather']]
     df_weather_final['day_plus'] = df_weather['day_plus'].apply(eval)
-    df_weather_final['felt_temperature'] = df_weather['felt_temperature'].apply(eval)
-    df_weather_final['rain_chances'] = df_weather['rain_chances'].apply(eval)
-    df_weather_final['humidity'] = df_weather['humidity'].apply(eval)
     df_weather_final[['lat', 'lon']] = df_weather[['lat', 'lon']].apply(pd.to_numeric)
 
     df_weather_full = df_weather_final.sort_values('score_weather')
     df_weather_full.reset_index(inplace=True, drop=True)
     df_weather_full = df_weather_full.apply(pd.Series.explode)
     df_weather_full[['day_plus', 'felt_temperature', 'rain_chances', 'humidity']] = df_weather_full[['day_plus', 'felt_temperature', 'rain_chances', 'humidity']].apply(pd.to_numeric)
-    # Creating a new column to be used to determine the size of the points in the plot
     df_weather_full['inverted_score'] = max(df_weather_full['score_weather']) - df_weather_full['score_weather']
     
     return df_weather_full
@@ -94,22 +86,28 @@ df = get_weather_data(df_s3)
 df_weather_full = weather_data(df)
 df_hotels_full = hotel_data(df)
 
-now = datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S")
+now_1 = datetime.datetime.now()
+now_string_1 = now_1.strftime("%d/%m/%Y %H:%M:%S")
+
 with st.sidebar:
-    'Click here to refresh the data from the Openweathermap API (takes about 10 sec)'
+    'Click here to refresh the data from OpenWeather (please once per hour maximum).'
     if st.button('Refresh Weather Data'):
+        now_2 = datetime.datetime.now()
         df_s3 = load_data()
         df = get_weather_data(df_s3)
         df_weather_full = weather_data(df)
         df_hotels_full = hotel_data(df)
-        st.write('Last refresh:', now)
+        now_1 = now_2
+        now_string_1 = now_1.strftime("%d/%m/%Y %H:%M:%S")
+        st.write('Last refresh:', now_string_1)
     else:
-        st.write('Last refresh:', now)
+        st.write('Last refresh:', now_string_1)
 
-'Duis reprehenderit nostrud labore et mollit quis occaecat commodo. Do labore quis ea velit esse occaecat aute sunt do. Sint veniam do id dolore officia quis amet. Aute velit sunt nulla anim. Exercitation veniam laboris enim culpa qui labore ut nulla incididunt dolore Lorem cupidatat pariatur amet. Ad officia consectetur dolor in commodo dolor irure voluptate sint elit velit.'
+st.write('This page shows the two plots required as deliverables for the Kayak Project, as part of the Jedha Bootcamp Fullstack training.')
+st.markdown('The repository of this project can be found [on this page](https://github.com/GuillaumeArp/Kayak_Project).')
 
 def plot_weather():
-    px.set_mapbox_access_token(open(".mapbox_token").read())
+    px.set_mapbox_access_token(mapbox_token)
 
     fig = px.scatter_mapbox(
         df_weather_full[:35],
@@ -144,7 +142,7 @@ def plot_weather():
     return fig
 
 def plot_hotels():
-    px.set_mapbox_access_token(open(".mapbox_token").read())
+    px.set_mapbox_access_token(mapbox_token)
 
     fig = px.scatter_mapbox(
         df_hotels_full[:100],
@@ -156,11 +154,12 @@ def plot_hotels():
         size_max=25,
         zoom=5,
         range_color = [min(df_hotels_full[:100]['score']), max(df_hotels_full[:100]['score'])],
-        hover_name='city',
+        hover_name='name',
         hover_data={
             'lat_hotels': False,
             'lon_hotels': False,
-            'name': True,
+            'name': False,
+            'city': True,
             'url': False,
             'description': False,
             'score_weather': False,
